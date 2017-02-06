51e18c6fb41ea9f8106c92b7832f91241644784f
Vinicius Pasquantonio <vinicius.pasquantonio@talentfour.com.br>
2017-02-05 22:09:45 -0200

    multiple components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
-export class Hero {
-  id: number;
-  name: string;
-}
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
   { id: 12, name: 'Narco' },
@@ -70,21 +67,14 @@ const HEROES: Hero[] = [
 `],
   template:`
 
-  	<h1>{{title}}</h1>
-  	<div *ngIf="selectedHero">
-	  <h2>{{selectedHero.name}} details!</h2>
-	  <div><label>id: </label>{{selectedHero.id}}</div>
-	  <div>
-	    <label>name: </label>
-	    <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-	  </div>
-	</div>
-	<h2>My Heroes</h2>
-	<ul class="heroes">
-		<li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
-			<span class="badge">{{hero.id}}</span> {{hero.name}}
-		</li>
-	</ul>
+  	<h1>{{title}}</h1>    
+  	<h2>My Heroes</h2>
+	  <ul class="heroes">
+		  <li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
+			  <span class="badge">{{hero.id}}</span> {{hero.name}}
+		  </li>
+	  </ul>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `
 })
 
