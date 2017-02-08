60b8e2bcd0f0386f04b3f93b6bfbab83ab6acc6a
Vinicius Pasquantonio <vinicius.pasquantonio@talentfour.com.br>
2017-02-08 16:34:08 -0200

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,53 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import { DashboardComponent }         from './dashboard.component';
 import {HeroDetailComponent} from './hero-detail.component';
+import { HeroService }         from './hero.service';
+import {HeroesComponent} from './heroes.component';
+
+
+
 
 @NgModule({
   imports:  [
   	BrowserModule,
-  	FormsModule 
+  	FormsModule ,
+  	RouterModule.forRoot([
+  		{
+		    path: '',
+		    redirectTo: '/dashboard',
+		    pathMatch: 'full'
+		  },
+  		{
+  			path: 'dashboard',
+  			component: DashboardComponent
+		  },
+      {
+        path: 'detail/:id',
+        component: HeroDetailComponent
+      },
+  		{
+    		path: 'heroes',
+    		component: HeroesComponent
+    	}
+  		
+	  ])
   ],
   declarations: [ 
   	AppComponent, 
-  	HeroDetailComponent
+  	DashboardComponent,
+  	HeroDetailComponent,
+  	HeroesComponent
+  ],
+  providers: [
+    HeroService
   ],
   bootstrap:    [ AppComponent ]
 })
+
 export class AppModule { }
+
--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,5 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params }   from '@angular/router';
+import { Location }                 from '@angular/common';
+
+import { HeroService } from './hero.service';
 import { Hero } from './hero';
+import 'rxjs/add/operator/switchMap';
 
 
 @Component({
@@ -13,11 +18,26 @@ import { Hero } from './hero';
       <input [(ngModel)]="hero.name" placeholder="name"/>
     </div>
   </div>
+  <button (click)="goBack()">Back</button>
 `
 
 })
-export class HeroDetailComponent {
-	@Input()
-	hero: Hero;
+export class HeroDetailComponent implements OnInit {
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+  @Input()
+  hero: Hero;
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
+  }
+  goBack(): void {
+    this.location.back();
+  }
+
 
 }
