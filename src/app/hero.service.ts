841eb5a987c205aa68e340a26c2a06f610bebe74
Vinicius Pasquantonio <vinicius.pasquantonio@gmail.com>
2017-02-12 22:23:00 -0200

    http!

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,13 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable }    from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heros';
 
 @Injectable()
 export class HeroService {
+	private heroesUrl = 'api/heroes';  // URL to web api
+	constructor(private http: Http) { }
   	getHeroes(): Promise<Hero[]> {
-  		return Promise.resolve(HEROES);
-	}
+    	return this.http.get(this.heroesUrl)
+               .toPromise()
+               .then(response => response.json().data as Hero[])
+               .catch(this.handleError);
+  	}
 	getHero(id: number): Promise<Hero> {
   		return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
 	}
@@ -18,6 +25,11 @@ export class HeroService {
   		}
   	);
 
+
+  }
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
   }
   
 }
